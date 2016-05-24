986e83e7980b6ca99b25fffe9844bc75f0cad01b
Andrii Vasyliev <sol@hiqdev.com>
2016-05-24 14:59:58 +0000

    fixed tests

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -14,6 +14,3 @@ error_reporting(E_ALL & ~E_NOTICE);
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
-foreach (require __DIR__ . '/../vendor/hiqdev/aliases.php' as $alias => $path) {
-    Yii::setAlias($alias, $path);
-}
