6b1133c745e47dc03c7dd3a2a45d485a0ece0ec7
Andrii Vasyliev <sol@hiqdev.com>
2017-02-15 13:41:14 +0000

    csfixed

--- src/config/hidev.php
+++ src/config/hidev.php
@@ -5,7 +5,7 @@
  * @link      https://github.com/hiqdev/hidev-nginx
  * @package   hidev-nginx
  * @license   BSD-3-Clause
- * @copyright Copyright (c) 2016, HiQDev (http://hiqdev.com/)
+ * @copyright Copyright (c) 2016-2017, HiQDev (http://hiqdev.com/)
  */
 
 return [
