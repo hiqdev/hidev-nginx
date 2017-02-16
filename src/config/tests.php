2705216740a7b2cc8aa58c62d9d1ba875e1617ff
Andrii Vasyliev <sol@hiqdev.com>
2017-02-16 08:25:42 +0000

    fixed tests

---
+++ src/config/tests.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * HiDev Nginx plugin
+ *
+ * @link      https://github.com/hiqdev/hidev-nginx
+ * @package   hidev-nginx
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2016-2017, HiQDev (http://hiqdev.com/)
+ */
+
+return [
+    'id' => 'hidev-nginx-tests',
+    'basePath' => dirname(__DIR__),
+];
--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -13,3 +13,8 @@ date_default_timezone_set('UTC');
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+
+use hiqdev\composer\config\Builder;
+use yii\console\Application;
+
+Yii::$app = new Application(require Builder::path('tests'));
